fd59549fdc988f9dd1d8ad5482d7f29ba0c4ca28
brighteyed <sergey.kondrikov@gmail.com>
2011-03-11 21:06:16 +0300

    Added rc-ido-mode.el

---
+++ emacs/rc/linux/rc-ido-mode.el
@@ -0,0 +1,10 @@
+;; Custom Emacs configuration
+;;
+;;   Sergey Kondrikov, sergey.kondrikov@gmail.com
+;;   2008-2011
+
+;; ido mode
+(ido-mode t)
+
+;; enable flex (fuzzy) matching
+(setq ido-enable-flex-matching t)
