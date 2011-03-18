0fe1de6c5e0488c2101bac6c6f412715eb138938
brighteyed <sergey.kondrikov@gmail.com>
2011-03-18 22:39:02 +0300

    Default frame font

--- emacs/linux/ecfg-custom.el
+++ emacs/linux/ecfg-custom.el
@@ -6,7 +6,7 @@
 (defvar ecfg-default-frame-rect '((top . 50) (left . 250) (width . 125) (height . 55))
   "Size and position of a window")
 
-(defvar ecfg-default-font-desc "-unknown-Liberation Mono-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1"
+(defvar ecfg-default-font-desc "Liberation Mono-9"
   "Default font")
 
 (defvar ecfg-default-org-file "~/secure/Dropbox/scratch.org"
