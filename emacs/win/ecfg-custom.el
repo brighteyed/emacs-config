a548919b0e78ca76a86d6ae3ea451390bcc1fe73
brighteyed <sergey.kondrikov@gmail.com>
2011-03-23 11:16:24 +0300

    Font settings

--- emacs/win/ecfg-custom.el
+++ emacs/win/ecfg-custom.el
@@ -3,18 +3,20 @@
 ;;   Sergey Kondrikov, sergey.kondrikov@gmail.com
 ;;   2008-2011
 
+;; common settings
+(defvar ecfg-default-font-desc "Courier New-10"
+  "Default font")
+
 ;; computer specific settings
 (when (string= system-name "BRIGHTEYED-WIN")
   (defvar ecfg-default-frame-rect '((top . 60) (left . 250) (width . 125) (height . 45))
-    "Size and position of a window"))
+    "Size and position of a window")
+  (defvar ecfg-default-org-file "C:/My Dropbox/scratch.org"
+    "Default org file"))
 
 ;; computer specific settings
 (when (string= system-name "SERGEY-K")
-  (defvar ecfg-default-frame-rect '((top . 20) (left . 150) (width . 132) (height . 60))
-    "Size and position of a window"))
-
-(defvar ecfg-default-font-desc "Consolas-10"
-  "Default font")
-
-(defvar ecfg-default-org-file "C:/My Dropbox/scratch.org"
-  "Default org file")
+  (defvar ecfg-default-frame-rect '((top . 20) (left . 140) (width . 125) (height . 55))
+    "Size and position of a window")
+  (defvar ecfg-default-org-file "C:/Users/kondrikov/Dropbox/scratch.org"
+    "Default org file"))
